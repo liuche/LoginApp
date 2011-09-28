c4bf557a5cc57408c74800d6f075bac7e0155c79
Philipp von Weitershausen <philipp@weitershausen.de>
2011-09-28 13:32:14 -0700

    Make importing Sync code work. Had to resort to programmatically setting resource aliases.

--- login-app/chrome/content/main.js
+++ login-app/chrome/content/main.js
@@ -1,4 +1,24 @@
-Components.utils.import("resource://services-sync/util.js");
+const Ci = Components.interfaces;
+const Cc = Components.classes;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+// Register resource aliases. Sadly these lines in the manifest wouldn't work:
+// resource services-sync resource://gre/modules/services-sync
+// resource services-crypto resource:/gre/modules/services-crypto
+function addResourceAlias() {
+  Cu.import("resource://gre/modules/Services.jsm");
+  const resProt = Services.io.getProtocolHandler("resource")
+                          .QueryInterface(Ci.nsIResProtocolHandler);
+  let uri;
+  uri = Services.io.newURI("resource://gre/modules/services-sync/", null, null);
+  resProt.setSubstitution("services-sync", uri);
+  uri = Services.io.newURI("resource://gre/modules/services-crypto/", null, null);
+  resProt.setSubstitution("services-crypto", uri);
+}
+addResourceAlias();
+
+Cu.import("resource://services-sync/util.js");
 
 function promptPassword() {
   let menu = document.getElementById("accounts-menu");
