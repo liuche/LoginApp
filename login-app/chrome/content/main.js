56dd5d1b0944396a64b74b16e4cb93c1a9b9cbfc
Chenxia Liu <liucheia@gmail.com>
2011-10-03 18:33:51 -0400

    creating new profiles per user

--- login-app/chrome/content/main.js
+++ login-app/chrome/content/main.js
@@ -21,46 +21,59 @@ addResourceAlias();
 Cu.import("resource://services-sync/util.js");
 Cu.import("resource://gre/modules/FileUtils.jsm");
 Cu.import("resource://gre/modules/NetUtil.jsm");
+Cu.import("resource://services-sync/service.js");
 
 const MENU_IMPORT = "Import Acount...";
 const MENU_GUEST = "Guest";
-let accounts;
+const DEF_PROFILE = "default";
+let accounts = [];
+let profileService = Cc["@mozilla.org/toolkit/profile-service;1"]
+                                .createInstance(Ci.nsIToolkitProfileService);
 
-// Load imported accounts to UI. 
+let prefs = Cc["@mozilla.org/preferences-service;1"]
+              .getService(Ci.nsIPrefBranch);
 
+// Load imported accounts into UI. 
 function loadAccounts() {
-  dump("LOADING ACCOUNTS\n");
+  profileService.selectedProfile = profileService.getProfileByName("default");
+  dump("current-profile:" + profileService.selectedProfile.name + "\n");
+  profileService.flush();
   let accountsfile = FileUtils.getFile("ProfD", ["data", "accounts.txt"]);
-  NetUtil.asyncFetch(accountsfile, function(inputStream, status) {
-    if (!Components.isSuccessCode(status)) {
-      dump("ERROR reading from file\n");
-      return;
-    }
-    accounts = NetUtil.readInputStreamToString(inputStream, inputStream.available()).split("\n");
-    accounts.pop();
-    dump("accounts:" + accounts + "\n");
-    makeMenu();
+  if (accountsfile.exists()) {
+    dump("is file\n");
+    NetUtil.asyncFetch(accountsfile, function(inputStream, status) {
+      if (Components.isSuccessCode(status)) {
+        accounts = NetUtil.readInputStreamToString(inputStream, inputStream.available()).split("\n");
+        accounts.pop();
+        dump("accounts:" + accounts + "\n");
+      } else {
+        dump("error reading from accounts-file\n");
+      }
+      dump("making menu\n");
+      makeMenu();
     });
+  } else {
+    dump("making menu\n");
+    makeMenu();
+  }
 }
 
+// Generates the UI account selection menu.
 function makeMenu() {
   dump("populating menu!!\n");
   let menu = document.getElementById("accounts-menu");
-  menu.removeAllItems(); // hack
-  menu.appendItem(MENU_GUEST);
   accounts.forEach(function(element) {
     dump("user: " + element);
     menu.appendItem(element);
   });
   menu.appendItem(MENU_IMPORT);
-  menu.selectedIndex = 0;
 }
 
 // Helper file write function
 function write(file, data) {
   dump("starting write\n");
   var ostream = FileUtils.openSafeFileOutputStream(file,
-                   FileUtils.MODE_CREATE |  FileUtils.MODE_APPEND);
+                   FileUtils.MODE_CREATE | FileUtils.MODE_WRONLY | FileUtils.MODE_APPEND);
   // TODO does not append! overwrites
   var converter = Cc["@mozilla.org/intl/scriptableunicodeconverter"].
                      createInstance(Ci.nsIScriptableUnicodeConverter);
@@ -72,12 +85,11 @@ function write(file, data) {
       dump("error writing\n");
       return;
     }
-    dump("written to file\n");
   });
   return;
 }
 
-// Add an account so it will be loaded next time.
+// Adds an account and writes it to file so it will be loaded next time.
 function importAccount() {
   let username = document.getElementById("add-input").value;
   let accountsfile = FileUtils.getFile("ProfD", ["data", "accounts.txt"]);
@@ -88,10 +100,13 @@ function importAccount() {
   }
   accounts.push(username);
   write(accountsfile, username + "\n");
-  dump("done\n");
+  dump("started async write\n");
+  profileService.createProfile(null, null, username);
+  profileService.flush();
 }
 
 // Process selection from login dropdown into UI changes. 
+// (password entry, username entry)
 function processSelection() {
   let menu = document.getElementById("accounts-menu");
   document.getElementById("add-row").hidden = true;
@@ -105,14 +120,14 @@ function processSelection() {
   }
 }
 
+// Login.
 function login() {
   let username = document.getElementById("accounts-menu").selectedItem.label;
   if (username == MENU_GUEST) {
+    // TODO create new profile, delete it afterwards
     window.open();
   } else { // Authenticate against sync account.
     if (username == MENU_IMPORT) {
-      dump("importing...\n");
-      importAccount();
       username = document.getElementById("add-input").value;
     }
     dump("authing...\n");
@@ -127,10 +142,7 @@ function login() {
 
     client.onreadystatechange = function handler() {
       // Sync node request succeeded.
-      // TODO: if fail before readystate 4 
-      dump("state:" + this.readyState);
-      dump( "/status:" + this.status);
-      dump("/statusText:" + this.statusText + "\n");
+      dump("state:" + this.readyState + "/status:" + this.status + "/statusText:" + this.statusText + "\n");
       if (this.readyState == 2 && this.status == 404) {
         alert("Username does not exist. Please try 'Create Account'.");
       } else if (this.readyState == 4 && this.status == 200) {
@@ -143,7 +155,11 @@ function login() {
             switch(this.status) {
               case 200: // Success!
                 dump("success!\n");
-                launchAccount().bind(this);
+                if (document.getElementById("accounts-menu").selectedItem.label == MENU_IMPORT) {
+                  dump("importing...\n");
+                  importAccount();
+                }
+                launchAccount();
                 break;
               case 401: // Unauthorized
                 alert("Incorrect Password");
@@ -162,3 +178,30 @@ function login() {
   }
 }
 
+function launchAccount() {
+  let username = "liuche+002@mozilla.com";
+  dump("starting user profile: " + username + "\n");
+  dump("fetching " + profileService.getProfileByName(username) + "\n");
+  profileService.selectedProfile = profileService.getProfileByName(username);
+  profileService.flush();
+  dump("loaded profile " + profileService.selectedProfile.name + "\n");
+  dump("homepage = ");
+  let homepage = prefs.getCharPref("browser.startup.homepage");
+  if (homepage == "") {
+    window.open();
+  } else {
+    dump("homepage: " + homepage + "\n");
+    window.open(homepage);
+  }
+}
+function test() {
+  dump("[profile-debug]\n");
+  let psIt = profileService.profiles;
+  dump("all profiles:\n");
+  while (psIt.hasMoreElements()) {
+    var elt = psIt.getNext();
+    dump("[" + elt.name + "/" + elt.localDir.leafName + "]\n");
+  }
+  dump("current profile:");
+  dump(profileService.selectedProfile.name + "\n");
+}
--- login-app/defaults/preferences/prefs.js
+++ login-app/defaults/preferences/prefs.js
@@ -1,5 +1,4 @@
 pref("toolkit.defaultChromeURI", "chrome://login-app/content/main.xul");
-pref("toolkit.singletonWindowType", "login-window");
 
 # no caching
 pref("nglayout.debug.disable_xul_cache", true);
